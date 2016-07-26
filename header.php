1302ea2a4bd5fee2ac2002e83ae089852873c26f
Alley Manalio <amanalio@ufl.edu>
2016-07-25 21:11:59 -0400

    Fix indentation issues

--- header.php
+++ header.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title><?php bloginfo(); ?></title>
-  <?php wp_head('name'); ?>
+    <title><?php bloginfo(); ?></title>
+    <?php wp_head('name'); ?>
 
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-  <div class="container">
-    <header class="row">
-      <div class="eleven columns">
-        <h1><a href="<?php $url = home_url('/'); echo $url; ?>">
-          <?php bloginfo('name'); ?></a></h1>
-        <h2><?php bloginfo('description'); ?></h2>
-      </div>
-      <div class="one column">
-        <p>hello</p>
-      </div>
-    </header>
-    <!-- MENU CODE BEGINS -->
-    <div class="row">
-      <div class="twelve columns">
-        <?php
-          wp_nav_menu( array (
-            'sort_column' => 'menu_order',
-            'container_class' => 'blank-menu-header'
-          ));
-        ?>
-      </div>
-    </div>
-    <!-- MENU CODE ENDS -->
+    <div class="container">
+        <header class="row">
+            <div class="eleven columns">
+                <h1><a href="<?php $url = home_url('/'); echo $url; ?>">
+                    <?php bloginfo('name'); ?></a></h1>
+                    <h2><?php bloginfo('description'); ?></h2>
+            </div>
+            <div class="one column">
+                <p>hello</p>
+            </div>
+        </header>
+        <!-- MENU CODE BEGINS -->
+        <div class="row">
+            <div class="twelve columns">
+                <?php
+                wp_nav_menu( array (
+                'sort_column' => 'menu_order',
+                'container_class' => 'blank-menu-header'
+                ));
+                ?>
+            </div>
+        </div>
+        <!-- MENU CODE ENDS -->
--- page.php
+++ page.php
@@ -1,27 +1,27 @@
 <?php get_header(); ?>
 
 <!-- Begin Section Container -->
-  <section class="row">
-    <div class="eight columns">
-      <!-- Begin Loop -->
-      <?php
-        if ( have_posts() ) {
-          while ( have_posts() ) {
-            the_post(); ?>
+    <section class="row">
+        <div class="eight columns">
+            <!-- Begin Loop -->
+            <?php
+            if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post(); ?>
 
-            <h2><?php the_title(); ?></h2>
-            <?php the_content();
-          }
-        }
-      ?>
-      <!-- End Loop -->
-    </div>
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content();
+                }
+            }
+            ?>
+            <!-- End Loop -->
+        </div>
 
-    <div class="four columns">
-      <?php get_sidebar(); ?>
-    </div>
+        <div class="four columns">
+          <?php get_sidebar(); ?>
+        </div>
 
-  </section>
+    </section>
 <!-- End Section Container -->
 
 <?php get_footer(); ?>
