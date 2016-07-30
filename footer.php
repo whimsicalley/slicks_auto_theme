618acbdcdc3a0852463caff472b6279b8d3c51ef
Alley Manalio <amanalio@ufl.edu>
2016-07-30 14:37:19 -0400

    Add container to header, nav, footer to center them

--- footer.php
+++ footer.php
@@ -1,13 +1,17 @@
     </div> <!-- End Container Div -->
-    <footer class="row">
-        <div class="six columns">
-            <?php dynamic_sidebar('footer-one'); ?>
-        </div>
-        <div class="six columns">
-            <?php dynamic_sidebar('footer-two'); ?>
-        </div>
-    </footer>
-    <!-- End Footer -->
+        <footer>
+            <div id="footer-container">
+                <div class="row">
+                    <div class="six columns">
+                        <?php dynamic_sidebar('footer-one'); ?>
+                    </div>
+                    <div class="six columns">
+                        <?php dynamic_sidebar('footer-two'); ?>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End Footer -->
     <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -8,27 +8,35 @@
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-    <header class="row">
-        <div class="nine columns header-left">
-            <h1><a href="<?php $url = home_url('/'); echo $url; ?>">
-                <?php bloginfo('name'); ?></a></h1>
-                <h2><?php bloginfo('description'); ?></h2>
-        </div>
-        <div class="three columns header-right">
-            <p>1234 Slick's Place<br />
-            Mays Landing, NJ<br /> 08330<br />
-            (609)555-9876</p>
+    <header>
+        <div class="header-container">
+            <div class="row">
+                <div class="nine columns header-left">
+                    <h1><a href="<?php $url = home_url('/'); echo $url; ?>">
+                        <?php bloginfo('name'); ?></a></h1>
+                        <h2><?php bloginfo('description'); ?></h2>
+                </div>
+                <div class="three columns header-right">
+                    <p>1234 Slick's Place<br />
+                    Mays Landing, NJ<br /> 08330<br />
+                    (609)555-9876</p>
+                </div>
+            </div>
         </div>
     </header>
     <!-- MENU CODE BEGINS -->
-    <nav class="row">
-        <div class="twelve columns">
-            <?php
-            wp_nav_menu( array (
-            'sort_column' => 'menu_order',
-            'container_class' => 'blank-menu-header'
-            ));
-            ?>
+    <nav>
+        <div class="header-container">
+            <div class="row">
+                <div class="twelve columns">
+                    <?php
+                    wp_nav_menu( array (
+                    'sort_column' => 'menu_order',
+                    'container_class' => 'blank-menu-header'
+                    ));
+                    ?>
+                </div>
+            </div>
         </div>
     </nav>
         <!-- MENU CODE ENDS -->
