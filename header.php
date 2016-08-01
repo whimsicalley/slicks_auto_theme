d324a2acbb870084905660aa1673688cbc4f2b29
Alley Manalio <amanalio@ufl.edu>
2016-08-01 17:46:33 -0400

    Add responsiveness to header and nav, fix styling issues

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <title><?php bloginfo(); ?></title>
     <?php wp_head('name'); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
